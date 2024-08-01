fac199dd991ce621a268daa8ccc380934e831201
Ju3336 <juana.ferreira.ju@gmail.com>
2024-08-01 09:03:52 -0300

    alteraçao const color

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
- import './App.css'
+import {useStage} from 'react'
+import './App.css'
  import {Routes,Route} from 'react-router-dom'
+ import Listagem from './pages/Listagem';
+
 
  const App = () => {
+const [color] = useStage("#0000FF")
+ document.body.style.backgroundColor = color
     return (
         <Routes>
-        <Routes path="/"/>
+        <Routes path="/" element ={<Listagem/>} />
         </Routes>
     )
 
